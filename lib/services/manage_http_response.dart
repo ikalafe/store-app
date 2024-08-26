bdfeb2bf0d7764676b8a81c64f46e3610474b15b
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-26 10:42:15 +0330

    Add manageHttpResponse function to handle HTTP response status codes and display messages
    - Implemented manageHttpResponse to manage different HTTP status codes (200, 201, 400, 500).
    - Added showSnackBar function to display messages in a SnackBar based on response status.
    - Provided callback execution on successful responses.

---
+++ lib/services/manage_http_response.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void manageHttpResponse({
+  required http.Response response, // the Http Response from the request
+  required BuildContext context, // the context is to sho snackbar
+  required VoidCallback
+      onSuccess, // the callback to excute on a successfull response
+}) {
+  // switch statement to handle different http status codes
+  switch (response.statusCode) {
+    case 200: // status code 200 indicates a successfull request
+      onSuccess();
+      break;
+    case 400: // status code 400 indicates bad request
+      showSnackBar(context, json.decode(response.body)['msg']);
+      break;
+    case 500: // status code 500 indicates a server error
+      showSnackBar(context, json.decode(response.body)['error']);
+      break;
+    case 201: // status code 201 indicates a resource was created successfully
+      onSuccess();
+      break;
+  }
+}
+
+void showSnackBar(BuildContext context, String title) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(title),
+    ),
+  );
+}
