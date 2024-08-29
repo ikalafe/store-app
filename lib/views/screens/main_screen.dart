bd2d34b1dd172ae90660f904ab3ff0dc22b5b870
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-29 06:54:11 +0330

    Integrate individual screens with Bottom Navigation Bar
    - Added navigation logic to switch between different screens  Home, Favorites, Stores, Cart, Account  in

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:mac_store_app/theme.dart';
+import 'package:mac_store_app/views/screens/nav_screen/account_screen.dart';
+import 'package:mac_store_app/views/screens/nav_screen/cart_screen.dart';
+import 'package:mac_store_app/views/screens/nav_screen/favorite_screen.dart';
+import 'package:mac_store_app/views/screens/nav_screen/home_screen.dart';
+import 'package:mac_store_app/views/screens/nav_screen/stores_screen.dart';
 
 class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
   @override
   State<MainScreen> createState() => _MainScreen();
 }
 
 class _MainScreen extends State<MainScreen> {
   int _pageIndex = 0;
+  final List<Widget> _pages = [
+    const HomeScreen(),
+    const FavoriteScreen(),
+    const StoresScreen(),
+    const CartScreen(),
+    const AccountScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,6 +74,7 @@ class _MainScreen extends State<MainScreen> {
               label: 'پروفایل',
             ),
           ]),
+          body: _pages[_pageIndex],
     );
   }
 }
