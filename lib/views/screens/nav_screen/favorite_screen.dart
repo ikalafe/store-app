bb0f663cfeac3a6641fae0f2f7aad25cfb56eb36
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-29 05:18:53 +0330

    Added favorites page for bottomAppBar

---
+++ lib/views/screens/nav_screen/favorite_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'صفحه علاقه مندی ها',
+        ),
+      ),
+    );
+  }
+}
