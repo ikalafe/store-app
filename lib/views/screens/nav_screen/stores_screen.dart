b4f902cecaa8b17813265679a988c2093ce49cbf
Daniyal <daniyaldehghan2021@gmail.com>
2024-08-29 06:34:29 +0330

    Added store_screen page for bottomAppBar

---
+++ lib/views/screens/nav_screen/stores_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StoresScreen extends StatelessWidget {
+  const StoresScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('صفحه فروشگاه'),
+      ),
+    );
+  }
+}
