bcb2a1eb17a34e7385341a6459faee796afd6764
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-11 16:55:25 +0330

    Add subcategory display functionality to CategoryScreen
    - Implemented functionality to load and display subcategories based on the selected category.
    - Clicking on a category now fetches and shows relevant subcategories.
    - Added a GridView to display subcategories with image and name.
    - Handled case when no subcategories are found with a placeholder message and image.

--- lib/views/screens/nav_screen/category_screen.dart
+++ lib/views/screens/nav_screen/category_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mac_store_app/controllers/category_controller.dart';
+import 'package:mac_store_app/controllers/subcategory_controller.dart';
 import 'package:mac_store_app/models/category_model.dart';
+import 'package:mac_store_app/models/subcategory_model.dart';
 import 'package:mac_store_app/views/screens/nav_screen/widgets/header_widget.dart';
+import 'package:mac_store_app/views/screens/nav_screen/widgets/image.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -13,10 +16,36 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   // A Future that wild hold the list of categories once loaded from the API
   late Future<List<CategoryModel>> futureCategories;
+  CategoryModel? _selectedCategory;
+  List<SubcategoryModel> _subcategories = [];
+  final SubcategoryController _subcategoryController = SubcategoryController();
   @override
   void initState() {
     super.initState();
     futureCategories = CategoryController().loadCategory();
+    // Once the Categories are loaded proccess then
+    futureCategories.then((categories) {
+      // Iterate through categories to find the "کفش مردانه" category
+      for (var category in categories) {
+        if (category.name == "کفش مردانه") {
+          // if "کفش مردانه" category is found, set it as the selected category
+          setState(() {
+            _selectedCategory = category;
+          });
+          // Load subcategories for the "کفش مردانه" category
+          _loadSubcategories(category.name);
+        }
+      }
+    });
+  }
+
+  // Load Subcategories base on the categoryName
+  Future<void> _loadSubcategories(String categoryName) async {
+    final subcategories = await _subcategoryController
+        .getSubcategoriesByCategoryName(categoryName);
+    setState(() {
+      _subcategories = subcategories;
+    });
   }
 
   @override
@@ -31,10 +60,10 @@ class _CategoryScreenState extends State<CategoryScreen> {
         children: [
           // Right side - Display categories
           Expanded(
-            flex: 2,
+            flex: 3,
             child: Container(
               decoration: const BoxDecoration(
-                color: Color(0xff5796E4),
+                color: Color.fromARGB(255, 78, 156, 252),
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(12),
                   bottomLeft: Radius.circular(12),
@@ -58,33 +87,34 @@ class _CategoryScreenState extends State<CategoryScreen> {
                       itemBuilder: (context, index) {
                         final category = categories[index];
                         return ListTile(
-                          title: Column(
-                            children: [
-                              Container(
-                                width: 100,
-                                padding: const EdgeInsets.only(
-                                  bottom: 1, // Space between underline and text
-                                ),
-                                decoration: BoxDecoration(
-                                  border: Border(
-                                    bottom: BorderSide(
-                                      color: Colors.yellow.shade300,
-                                      width: 1.5, // Underline thickness
-                                    ),
-                                  ),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    category.name,
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Dana'),
-                                  ),
+                          onTap: () {
+                            setState(() {
+                              _selectedCategory = category;
+                            });
+                            _loadSubcategories(category.name);
+                          },
+                          title: Container(
+                            width: 80,
+                            height: _selectedCategory == category ? 50 : null,
+                            decoration: BoxDecoration(
+                              borderRadius: _selectedCategory == category
+                                  ? BorderRadius.circular(12)
+                                  : null,
+                              color: _selectedCategory == category
+                                  ? const Color.fromARGB(255, 11, 18, 162)
+                                  : null,
+                            ),
+                            child: Center(
+                              child: Text(
+                                category.name,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Dana',
                                 ),
                               ),
-                            ],
+                            ),
                           ),
                         );
                       },
@@ -93,7 +123,103 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 },
               ),
             ),
-          )
+          ),
+          // Left Side - Display slelected cateogry detailes
+          Expanded(
+            flex: 6,
+            child: _selectedCategory != null
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'دسته بندی: ${_selectedCategory!.name}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      _subcategories.isNotEmpty
+                          ? GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: _subcategories.length,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                crossAxisSpacing: 1,
+                                mainAxisSpacing: 10,
+                              ),
+                              itemBuilder: (context, index) {
+                                final subcategory = _subcategories[index];
+                                debugPrint('$subcategory');
+                                return Column(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Color(0xffEFF6FF),
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                      width: 110,
+                                      height: 120,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          ImageLoadingService(
+                                            imageUrl: subcategory.image,
+                                            widthImage: 80,
+                                            heithImage: 80,
+                                            imageBorderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            subcategory.subCategoryName,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                );
+                              },
+                            )
+                          : SizedBox(
+                              height: 500,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'زیر مجموعه دسته بندی یافت نشد.',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                      color: Colors.grey.shade600,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  Image.asset(
+                                    'assets/images/notfound.png',
+                                    width: 150,
+                                    height: 150,
+                                  ),
+                                ],
+                              ),
+                            ),
+                    ],
+                  )
+                : Container(),
+          ),
         ],
       ),
     );
