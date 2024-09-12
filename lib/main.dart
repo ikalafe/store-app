4dfbf253924be28ab41ce334b82ecc0748615cbd
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-12 15:53:20 +0330

    Add token validation and user state initialization in main.dart
    - Implement _checkTokenAndSetUser method to validate token and set user state using SharedPreferences.
    - Update FutureBuilder to check for user data and navigate accordingly to MainScreen or LoginScreen.
    - Enhance app initialization by managing authentication state with Riverpod.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mac_store_app/provider/user_provider.dart';
 import 'package:mac_store_app/theme.dart';
-import 'package:mac_store_app/views/screens/authentication_screens/register_screen.dart';
+import 'package:mac_store_app/views/screens/authentication_screens/login_screen.dart';
+import 'package:mac_store_app/views/screens/main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,12 +14,27 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+//Root widget of the application, a consummerWidget to consume state change
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
+
+  //Method to check the token and set the user data if availible
+  Future<void> _checkTokenAndSetUser(WidgetRef ref) async {
+    //obtain an instance of sharedPreference for local data storage
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    // Retrive the authentication token and user data stored locally.
+    String? token = preferences.getString('auth_token');
+    String? userJson = preferences.getString('user');
+
+    // if both token and user data are availble, update the user state
+    if (token != null && userJson != null) {
+      ref.read(userProvider.notifier).setUser(userJson);
+    }
+  }
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     const defaultTextStyle = TextStyle(
       fontFamily: 'IranYekan',
       color: LightThemeColors.primaryTextColor,
@@ -25,21 +43,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Store App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         fontFamily: 'Dana',
         textTheme: TextTheme(
           bodyLarge: defaultTextStyle,
@@ -60,9 +63,20 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const Directionality(
+      home: Directionality(
         textDirection: TextDirection.rtl,
-        child: RegisterScreen(),
+        child: FutureBuilder(
+          future: _checkTokenAndSetUser(ref),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final user = ref.watch(userProvider);
+            return user != null ? const MainScreen() : const LoginScreen();
+          },
+        ),
       ),
     );
   }
