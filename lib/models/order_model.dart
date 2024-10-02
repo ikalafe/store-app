db659671cc6a546fc4ad7d7ae2a2a2a1641f7c57
Daniyal <daniyaldehghan2021@gmail.com>
2024-10-02 03:48:15 +0330

    Created OrderModel for handling order data.
    - Defined properties including id, customer details, product details, and order status (processing/delivered).
    - Added methods to convert OrderModel to and from Map and JSON for data serialization.

---
+++ lib/models/order_model.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class OrderModel {
+  final String id;
+  final String fullName;
+  final String email;
+  final String state;
+  final String city;
+  final String locality;
+  final String productName;
+  final int productPrice;
+  final int quantity;
+  final String category;
+  final String image;
+  final String buyerId;
+  final String vendorId;
+  final bool processing;
+  final bool delivered;
+
+  OrderModel({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.state,
+    required this.city,
+    required this.locality,
+    required this.productName,
+    required this.productPrice,
+    required this.quantity,
+    required this.category,
+    required this.image,
+    required this.buyerId,
+    required this.vendorId,
+    required this.processing,
+    required this.delivered,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'fullName': fullName,
+      'email': email,
+      'state': state,
+      'city': city,
+      'locality': locality,
+      'productName': productName,
+      'productPrice': productPrice,
+      'quantity': quantity,
+      'category': category,
+      'image': image,
+      'buyerId': buyerId,
+      'vendorId': vendorId,
+      'processing': processing,
+      'delivered': delivered,
+    };
+  }
+
+  factory OrderModel.fromMap(Map<String, dynamic> map) {
+    return OrderModel(
+      id: map['id'] as String,
+      fullName: map['fullName'] as String,
+      email: map['email'] as String,
+      state: map['state'] as String,
+      city: map['city'] as String,
+      locality: map['locality'] as String,
+      productName: map['productName'] as String,
+      productPrice: map['productPrice'] as int,
+      quantity: map['quantity'] as int,
+      category: map['category'] as String,
+      image: map['image'] as String,
+      buyerId: map['buyerId'] as String,
+      vendorId: map['vendorId'] as String,
+      processing: map['processing'] as bool,
+      delivered: map['delivered'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory OrderModel.fromJson(String source) => OrderModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
