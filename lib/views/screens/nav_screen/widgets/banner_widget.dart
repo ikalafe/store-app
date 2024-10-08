a742f6d01366e6e6b775ad14abec55f3521cb411
Daniyal <daniyaldehghan2021@gmail.com>
2024-10-09 02:21:22 +0330

    Refactored BannerWidget to use provider instead of FutureBuilder
    - Updated BannerWidget in lib/views/screens/nav_screen/widgets/banner_widget.dart to use Riverpod for managing banner state.
    - Removed FutureBuilder and replaced it with BannerProvider for better performance and state management.
    - Fetched banners using the _fetchBanners method in initState and updated the provider state.

--- lib/views/screens/nav_screen/widgets/banner_widget.dart
+++ lib/views/screens/nav_screen/widgets/banner_widget.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mac_store_app/controllers/banner_controller.dart';
 import 'package:mac_store_app/models/banner_model.dart';
+import 'package:mac_store_app/provider/banner_provider.dart';
 import 'package:mac_store_app/views/screens/nav_screen/widgets/image.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class BannerWidget extends StatefulWidget {
+class BannerWidget extends ConsumerStatefulWidget {
   const BannerWidget({super.key});
 
   @override
-  State<BannerWidget> createState() => _BannerWidgetState();
+  ConsumerState<BannerWidget> createState() => _BannerWidgetState();
 }
 
-class _BannerWidgetState extends State<BannerWidget> {
+class _BannerWidgetState extends ConsumerState<BannerWidget> {
   final PageController pageController = PageController();
-  // A Future the will hold the list of banners once loaded from the API
-  late Future<List<BannerModel>> futureBanners;
+
   @override
   void initState() {
     super.initState();
-    futureBanners = BannerController().loadBanners();
+    _fetchBanners();
+  }
+
+  Future<void> _fetchBanners() async {
+    final BannerController bannerController = BannerController();
+    try {
+      final banners = await bannerController.loadBanners();
+      ref.read(bannerProvider.notifier).setBanners(banners);
+    } catch (e) {
+      print('Error fetch banners: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final banners = ref.watch(bannerProvider);
     return Padding(
       padding: const EdgeInsets.only(right: 0, left: 0),
       child: Container(
@@ -32,80 +44,59 @@ class _BannerWidgetState extends State<BannerWidget> {
           color: Colors.transparent,
           borderRadius: BorderRadius.circular(12),
         ),
-        child: FutureBuilder(
-          future: futureBanners,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('مشکلی در بارگزاری بنر رخ داد: ${snapshot.error}'),
-              );
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text('بنری یافت نشد ):'),
-              );
-            } else {
-              final banners = snapshot.data!;
-              return AspectRatio(
-                aspectRatio: 2,
-                child: Stack(
-                  children: [
-                    PageView.builder(
-                      controller: pageController,
-                      itemCount: banners.length,
-                      itemBuilder: (context, index) {
-                        final banner = banners[index];
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 12, right: 12),
-                          child: ImageLoadingService(
-                            imageBorderRadius: BorderRadius.circular(12),
-                            imageUrl: banner.image,
-                          ),
-                        );
-                      },
+        child: AspectRatio(
+          aspectRatio: 2,
+          child: Stack(
+            children: [
+              PageView.builder(
+                controller: pageController,
+                itemCount: banners.length,
+                itemBuilder: (context, index) {
+                  final banner = banners[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 12, right: 12),
+                    child: ImageLoadingService(
+                      imageBorderRadius: BorderRadius.circular(12),
+                      imageUrl: banner.image,
                     ),
-                    Positioned(
-                      left: 145,
-                      right: 145,
-                      bottom: 0,
-                      child: Container(
-                        width: 101,
-                        height: 13,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(4),
-                            topRight: Radius.circular(4),
-                          ),
-                        ),
-                        child: Center(
-                          child: SmoothPageIndicator(
-                            controller: pageController,
-                            count: banners.length,
-                            axisDirection: Axis.horizontal,
-                            effect: const ExpandingDotsEffect(
-                              paintStyle: PaintingStyle.fill,
-                              dotColor: Color(0xffB6D6FD),
-                              activeDotColor: Color(0xff4475B2),
-                              spacing: 3.0,
-                              dotHeight: 4,
-                              radius: 4,
-                              dotWidth: 8,
-                            ),
-                          ),
-                        ),
+                  );
+                },
+              ),
+              Positioned(
+                left: 145,
+                right: 145,
+                bottom: 0,
+                child: Container(
+                  width: 101,
+                  height: 13,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(4),
+                      topRight: Radius.circular(4),
+                    ),
+                  ),
+                  child: Center(
+                    child: SmoothPageIndicator(
+                      controller: pageController,
+                      count: banners.length,
+                      axisDirection: Axis.horizontal,
+                      effect: const ExpandingDotsEffect(
+                        paintStyle: PaintingStyle.fill,
+                        dotColor: Color(0xffB6D6FD),
+                        activeDotColor: Color(0xff4475B2),
+                        spacing: 3.0,
+                        dotHeight: 4,
+                        radius: 4,
+                        dotWidth: 8,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              );
-            }
-          },
+              ),
+            ],
+          ),
         ),
-      
       ),
     );
   }
